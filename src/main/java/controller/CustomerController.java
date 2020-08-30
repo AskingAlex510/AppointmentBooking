37f713ef3c7a845fde646e5b694fd2dec5ba5cfd
benjigeorge <s3740923@rmit.edu.vn>
2020-08-30 15:58:16 +0700

    Finished the basic aesthetic layout of the primary Login Page.

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -9,9 +9,11 @@ import java.util.List;
 
 /**
  * Created by CoT on 7/29/18.
+ *
+ *
  */
-@RestController
-@RequestMapping(path = "/")
+@RestController //@Controller + @ResponseBody (The @ResponseBody annotation tells a controller that the object returned is automatically serialized into JSON and passed back into the HttpResponse object.)
+@RequestMapping(path = "/")//Maps the controller to the URI.
 public class CustomerController {
 
     @Autowired
@@ -21,17 +23,17 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @RequestMapping(path = "customers", method = RequestMethod.GET)
+    @GetMapping("customers")//GET returns a representation in XML or JSON and an HTTP response code of 200 (OK). GET requests are used only to read data and not change it.
     public List<Customer> getAllCustomers(){
         return customerService.getAllCustomers();
     }
 
-    @RequestMapping(path = "customers", method = RequestMethod.POST)
+    @PostMapping("customers")//The POST verb is most-often utilized to create new resources. In particular, it's used to create subordinate resources. That is, subordinate to some other (e.g. parent) resource. In other words, when creating a new resource, POST to the parent and the service takes care of associating the new resource with the parent, assigning an ID (new resource URI), etc.
     public void saveTeacher(@RequestBody Customer customer){
         customerService.saveCustomer(customer);
     }
 
-    @RequestMapping(path = "customers/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("customers/{id}")//It is used to delete a resource identified by a URI.
     public void saveTeacher(@PathVariable int id){
         customerService.deleteCustomer(id);
     }
